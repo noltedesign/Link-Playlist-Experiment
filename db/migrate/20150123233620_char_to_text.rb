76380e06c08f590229824569bcff0e5be7d6437c
Drew N <noltedesign@gmail.com>
2015-04-11 20:28:48 -0600

    remove change title

--- db/migrate/20150123233620_char_to_text.rb
+++ db/migrate/20150123233620_char_to_text.rb
@@ -1,6 +1,5 @@
 class CharToText < ActiveRecord::Migration
   def change
-    change_column :feed_items, :title, :text
     change_column :feed_items, :item_url, :text
     change_column :feed_items, :guid, :text
   end
