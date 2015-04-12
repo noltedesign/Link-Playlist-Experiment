d69a9a9f71e8d332b33d2eaffe000846a32614d2
Drew N <noltedesign@gmail.com>
2015-04-11 20:06:23 -0600

    update feed cat migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,50 +11,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219200609) do
+ActiveRecord::Schema.define(version: 20150123233620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "feed_items", force: true do |t|
-    t.string   "feed_id"
-    t.string   "title",        limit: 400
+  create_table "feed_categories", force: :cascade do |t|
+    t.string "category_name", limit: 255
+  end
+
+  create_table "feed_categorizations", force: :cascade do |t|
+    t.integer "feed_id"
+    t.integer "feed_category_id"
+    t.string  "category_order",   limit: 255
+  end
+
+  create_table "feed_items", force: :cascade do |t|
+    t.string   "feed_id",      limit: 255
+    t.text     "title"
     t.text     "summary"
-    t.string   "item_url"
-    t.string   "published_on"
-    t.string   "guid"
+    t.text     "item_url"
+    t.string   "published_on", limit: 255
+    t.text     "guid"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "feed_relation", force: true do |t|
-    t.string "feed_id"
-    t.string "user_id"
+  create_table "feeds", force: :cascade do |t|
+    t.string "link",       limit: 255
+    t.string "feed_title", limit: 255
+    t.string "feed_link",  limit: 255
+    t.string "feed_type",  limit: 255
   end
 
-  create_table "feeds", force: true do |t|
-    t.string "user_id"
-    t.string "link"
-    t.string "order"
-    t.string "feed_title"
-    t.string "feed_link"
-    t.string "feed_type"
-  end
-
-  create_table "saved_items", force: true do |t|
-    t.string   "user_id"
+  create_table "saved_items", force: :cascade do |t|
+    t.string   "user_id",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "feed_item_id"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email"
-    t.string   "password_hash"
+  create_table "user_feeds", force: :cascade do |t|
+    t.integer "feed_id"
+    t.integer "user_id"
+    t.string  "feed_order", limit: 255
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",         limit: 255
+    t.string   "password_hash", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "page_title"
-    t.string   "userurl",       limit: nil
+    t.string   "page_title",    limit: 255
+    t.string   "userurl"
   end
 
 end
