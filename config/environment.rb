486a0d81cb190ff74a0e32ac8595c00549d28b17
Drew N <noltedesign@gmail.com>
2015-04-11 19:43:54 -0600

    update gems

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,14 @@
 #configure DB
 
-db = URI.parse('postgres://tart_user:drew1234@localhost/poptart')
-
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :password => db.password,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
+configure :development do
+  db = URI.parse('postgres://tart_user:drew1234@localhost/poptart')
+  
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
